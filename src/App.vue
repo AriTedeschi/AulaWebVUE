8ddda2cc705a76c8e808653e040987bc9fc9629d
AriTedeschi <arijun2010@hotmail.com>
2021-11-04 20:22:08 -0300

    1-portuguese translation

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Tela inicial</router-link> |
+      <router-link to="/about">Sobre</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+body{
+  background-color:#0d2f57;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
 }
 
 #nav {
@@ -23,10 +26,10 @@
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: ##d6861e;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #d6861e;
 }
 </style>
